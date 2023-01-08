c8353a763939cb6818eeb42519675104784e5dc3
Matthew Gitata <matthewgitata@gmail.com>
2023-01-08 18:45:00 +0300

    Reverse a stream of data.

---
+++ src/com/matthewgitata/katas/datareverse/DataReverse.java
@@ -0,0 +1,19 @@
+package com.matthewgitata.katas.datareverse;
+
+/**
+ * The {@code DataReverse} class contains method to
+ * reverse a stream of data. The data contains 8 bits
+ * long segments, whose order needs to be reversed.
+ */
+public class DataReverse {
+
+    /**
+     * Reverses a stream of data
+     *
+     * @param data stream of data
+     * @return the reversed stream of data, {@code data}
+     */
+    public static int[] dataReverse(int[] data) {
+        return new int[]{};
+    }
+}
