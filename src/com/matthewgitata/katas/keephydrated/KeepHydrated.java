9ea60b4d3205bb04e926cb56aac123ad366d30cc
Matthew Gitata <matthewgitata@gmail.com>
2023-01-10 23:59:27 +0300

    Find the number of litres drank.

---
+++ src/com/matthewgitata/katas/keephydrated/KeepHydrated.java
@@ -0,0 +1,15 @@
+package com.matthewgitata.katas.keephydrated;
+
+/**
+ * The {@code  KeepHydrated} class contains a method to
+ * find out number of litres of water drunk per coding
+ * hour.
+ *
+ * created by @matthewgitata on 01/10/2023
+ */
+
+public class KeepHydrated  {
+    public int Liters(double time)  {
+        return (int) (time * 0.5);
+    }
+}
