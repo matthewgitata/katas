5b7468f86496db96fb2dfa7b1d8d3895a3bbb4d9
Matthew Gitata <matthewgitata@gmail.com>
2023-01-11 23:59:00 +0300

    Find the odd number from the rest.

---
+++ src/com/matthewgitata/katas/uniquenumber/UniqueNumber.java
@@ -0,0 +1,18 @@
+package com.matthewgitata.katas.uniquenumber;
+
+/**
+ * The {@code UniqueNumber} class contains a method
+ * that returns the number not equal to the rest.
+ * <p>
+ * created by @matthewgitata on 11/01/2023.
+ */
+public class UniqueNumber {
+    /**
+     * @param arr array of numbers
+     * @return the unique number in {@code arr}
+     */
+    public static double findUniq(double arr[]) {
+        // Do the magic
+        return arr[0];
+    }
+}
